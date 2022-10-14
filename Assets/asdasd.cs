7813c3a511a6741ba557aef3346226c0d61186f7
artsung410 <zpzpskm@naver.com>
2022-10-14 15:45:26 +0900

    맵 수정

---
+++ Assets/asdasd.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+    // ###############################################
+    //             NAME : ARTSUNG                      
+    //             MAIL : artsung410@gmail.com         
+    // ###############################################
+
+public class asdasd : MonoBehaviour
+{
+    [SerializeField]
+    Transform tg;
+    NavMeshAgent NavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMeshAgent.SetDestination(tg.position);
+    }
+}
