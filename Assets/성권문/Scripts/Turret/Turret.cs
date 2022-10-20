baf5c93681d28867e4b5681bb798e3e5af4ebca6
SungKwonMoon <zpzpskm@naver.com>
2022-10-20 15:54:57 +0900

    [Feat] 타워 파괴 / 애니메이션 구현

--- Assets/성권문/Scripts/Turret/Turret.cs
+++ Assets/성권문/Scripts/Turret/Turret.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 
 // ###############################################
 //             NAME : ARTSUNG                      
 //             MAIL : artsung410@gmail.com         
 // ###############################################
 
-public abstract class Turret : MonoBehaviourPun
+public class Turret : MonoBehaviourPun
 {
-    public int Hp;
+    public float currentHealth;
+    public float maxHealth;
+    public Image healthbarImage;
+    public GameObject ui;
+    public GameObject destroyParticle;
+    public float destorySpeed;
 
     [Header("타겟 TAG")]
     public string enemyTag;
 
     protected void OnEnable()
     {
+        currentHealth = maxHealth;
+
         if (PhotonNetwork.IsMasterClient)
         {
             if (PhotonNetwork.LocalPlayer.ActorNumber == 1 && photonView.IsMine)
@@ -48,14 +56,46 @@ public abstract class Turret : MonoBehaviourPun
         }
     }
 
-    public void TakeDamage(int Damage)
+    public void Damage(float damage)
     {
-        if (Damage <= 0)
+        Debug.Log("Damage 적용");
+        photonView.RPC("TakeDamage", RpcTarget.All, damage);
+    }
+
+    [PunRPC]
+    public void TakeDamage(float damage)
+    {
+        Debug.Log("Damage RPC적용");
+
+        currentHealth -= damage;
+        healthbarImage.fillAmount = currentHealth / maxHealth;
+
+        if (currentHealth <= 0)
         {
-            Destroy(gameObject);
+            Destroy();
             return;
         }
+    }
 
-        Hp -= Damage;
+    public void Destroy()
+    {
+        StartCoroutine(Destructing());
+        StartCoroutine(Destruction(PhotonNetwork.Instantiate(destroyParticle.name, new Vector3(transform.position.x, transform.position.y + 3, transform.position.z), transform.rotation)));
+    }
+
+    IEnumerator Destructing()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(0.01f);
+            transform.Translate(Vector3.down * Time.deltaTime * destorySpeed);
+        }
+    }
+
+    IEnumerator Destruction(GameObject particle)
+    {
+        yield return new WaitForSeconds(1.5f);
+        PhotonNetwork.Destroy(particle);
+        PhotonNetwork.Destroy(gameObject);
     }
 }
