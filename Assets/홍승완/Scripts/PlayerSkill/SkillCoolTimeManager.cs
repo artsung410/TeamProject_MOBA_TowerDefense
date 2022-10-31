c39acf54e5e1458f278f3a694e989243539f2925
usrrt <gkenfktm@gmail.com>
2022-10-31 17:44:23 +0900

    [Feat] 캐릭터 사망시에도 스킬 쿨타임 적용

---
+++ Assets/홍승완/Scripts/PlayerSkill/SkillCoolTimeManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillCoolTimeManager : MonoBehaviour
+{
+    // ###############################################
+    //             NAME : HongSW                      
+    //             MAIL : gkenfktm@gmail.com         
+    // ###############################################
+
+    public Image[] coolTimeImgs;
+    public bool[] isCoolDown;
+    float[] skillCoolTimeArr;
+
+    private void OnEnable()
+    {
+        coolTimeImgs = new Image[4];
+        isCoolDown = new bool[4];
+        skillCoolTimeArr = new float[4];
+    }
+
+    void Start()
+    {
+        StartCoroutine(Init());
+    }
+
+    IEnumerator Init()
+    {
+        yield return new WaitForSeconds(0.5f);
+        for (int i = 0; i < 4; i++)
+        {
+            coolTimeImgs[i] = PlayerHUD.Instance.skillTable.transform.GetChild(i).GetChild(2).gameObject.GetComponent<Image>();
+            skillCoolTimeArr[i] = SkillManager.Instance.time[i];
+        }
+    }
+
+    private void Update()
+    {
+        CoolTimeCheckQ();
+        CoolTimeCheckW();
+        CoolTimeCheckE();
+        CoolTimeCheckR();
+    }
+
+    public void CoolTimeCheckQ()
+    {
+        if (isCoolDown[0])
+        {
+            coolTimeImgs[0].fillAmount -= 1 / skillCoolTimeArr[0] * Time.deltaTime;
+
+            if (coolTimeImgs[0].fillAmount <= 0f)
+            {
+                coolTimeImgs[0].fillAmount = 0f;
+                isCoolDown[0] = false;
+            }
+
+        }
+
+
+    }
+
+    public void CoolTimeCheckW()
+    {
+        if (isCoolDown[1])
+        {
+            coolTimeImgs[1].fillAmount -= 1 / skillCoolTimeArr[1] * Time.deltaTime;
+
+            if (coolTimeImgs[1].fillAmount <= 0f)
+            {
+                coolTimeImgs[1].fillAmount = 0f;
+                isCoolDown[1] = false;
+            }
+        }
+    }
+
+    public void CoolTimeCheckE()
+    {
+        if (isCoolDown[2])
+        {
+            coolTimeImgs[2].fillAmount -= 1 / skillCoolTimeArr[2] * Time.deltaTime;
+
+            if (coolTimeImgs[2].fillAmount <= 0f)
+            {
+                coolTimeImgs[2].fillAmount = 0f;
+                isCoolDown[2] = false;
+            }
+        }
+    }
+
+    public void CoolTimeCheckR()
+    {
+        if (isCoolDown[3])
+        {
+            coolTimeImgs[3].fillAmount -= 1 / skillCoolTimeArr[3] * Time.deltaTime;
+
+            if (coolTimeImgs[3].fillAmount <= 0f)
+            {
+                coolTimeImgs[3].fillAmount = 0f;
+                isCoolDown[3] = false;
+            }
+        }
+    }
+}
