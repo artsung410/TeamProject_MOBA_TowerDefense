d80f0d5587de87ce50e3a15af5ba5e281a558454
Minsub Sim <minsub4400@naver.com>
2022-11-15 17:44:36 +0900

    Commit 02
    - Retry 버튼 구현
    - 데이터 세분화

--- Assets/심민섭/Scripts/Buttons/OpenDrawBox.cs
+++ Assets/심민섭/Scripts/Buttons/OpenDrawBox.cs
@@ -33,4 +33,14 @@ public class OpenDrawBox : MonoBehaviour
         closeObj.SetActive(false);
         openObj.SetActive(true);
     }
+
+    public void RetryButton()
+    {
+        // 아이템 소모
+        DrawManager.instance.OpenBoxDisCount();
+        // 카드 뽑기 창 초기화
+        DrawManager.instance.CardDataInit();
+        // 카드 뽑기 실행
+        randomSelect.SkillResultSelect();
+    }
 }
