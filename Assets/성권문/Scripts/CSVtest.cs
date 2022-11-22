0a3891b3110d921f3b3b6eb0cdaf702ea910da92
SungKwonMoon <zpzpskm@naver.com>
2022-11-22 14:39:23 +0900

    fix

--- Assets/성권문/Scripts/CSVtest.cs
+++ Assets/성권문/Scripts/CSVtest.cs
@@ -6,10 +6,13 @@ using System.IO;
 
 public class CSVtest : MonoBehaviour
 {
-    private const string URL = "https://docs.google.com/spreadsheets/d/1Qlp1QXfwrp5AdR5SE1El6H0OL8pm3vq_/edit#gid=625995306/export?format=tsv&range=A5:AL124";
+    private const string URL = "https://docs.google.com/spreadsheets/d/1FOm8D4Hb0IbgmNOnSLiLrV7HpSgB-kjS/export?format=tsv&gid=625995306&range=A5:AM124";
+                               
     private string MYtext = "";
 
-    public List<Ghost> ghosts;
+    [Header("[타워]")]
+    public TowerDatabaseList towerDatabaseList_Ingame;
+    public ItemDataBaseList towerDatabaseList_Robby;
 
     private void Start()
     {
@@ -25,38 +28,69 @@ public class CSVtest : MonoBehaviour
         yield return GetCharactorData.SendWebRequest();
         Debug.Log(elaspedTime);
 
-        //MYtext = GetCharactorData.downloadHandler.text;
-        //Debug.Log(MYtext);
-        //SetCharactorDatas(MYtext);
+        MYtext = GetCharactorData.downloadHandler.text;
+        Debug.Log(MYtext);
+        SetCharactorDatas(MYtext);
     }
 
-    private void Update()
+    public void SetCharactorDatas(string tsv)
     {
-        elaspedTime += Time.deltaTime;
-        if(GetCharactorData.isDone)
-        {  
-            Debug.Log($"{elaspedTime}");
+        string[] row = tsv.Split('\n');
+        int rowSize = row.Length;
+        int columnSize = row[0].Split('\t').Length;
+
+        for (int i = 0; i < rowSize; i++)
+        {
+            string[] col = row[i].Split('\t');   
+
+            // 기본정보
+            towerDatabaseList_Ingame.itemList[i].ID = int.Parse(col[0]);
+            towerDatabaseList_Ingame.itemList[i].Pf = Resources.Load<GameObject>(col[3]);
+            towerDatabaseList_Ingame.itemList[i].Name = col[4];
+
+            // 조합
+            towerDatabaseList_Ingame.itemList[i].Combination_ResultID = int.Parse(col[6]);
+            towerDatabaseList_Ingame.itemList[i].Combination_Required_Value = int.Parse(col[7]);
+
+            // 뽑기
+            towerDatabaseList_Ingame.itemList[i].Normal_Random_Draw_Probability = float.Parse(col[8]);
+            towerDatabaseList_Ingame.itemList[i].Normal_Attack_Draw_Probability = float.Parse(col[9]);
+            towerDatabaseList_Ingame.itemList[i].Normal_Minion_Draw_Probability = float.Parse(col[10]);
+            towerDatabaseList_Ingame.itemList[i].Normal_Buff_Debuff_Draw_Probability = float.Parse(col[11]);
+
+            towerDatabaseList_Ingame.itemList[i].Premium_Random_Draw_Probability = float.Parse(col[12]);
+            towerDatabaseList_Ingame.itemList[i].Premium_Attack_Draw_Probability = float.Parse(col[13]);
+            towerDatabaseList_Ingame.itemList[i].Premium_Minion_Draw_Probability = float.Parse(col[14]);
+            towerDatabaseList_Ingame.itemList[i].Premium_Buff_Debuff_Draw_Probability = float.Parse(col[15]);
+
+            // 속성
+            towerDatabaseList_Ingame.itemList[i].GroupID = int.Parse(col[16]);
+            towerDatabaseList_Ingame.itemList[i].Rank = int.Parse(col[17]);
+            towerDatabaseList_Ingame.itemList[i].Type = int.Parse(col[18]);
+            towerDatabaseList_Ingame.itemList[i].Attack = float.Parse(col[19]);
+            towerDatabaseList_Ingame.itemList[i].Attack_Speed = float.Parse(col[20]);
+            towerDatabaseList_Ingame.itemList[i].Hp = int.Parse(col[21]);
+            towerDatabaseList_Ingame.itemList[i].Range = int.Parse(col[22]);
+            towerDatabaseList_Ingame.itemList[i].Range_Type = int.Parse(col[23]);
+
+            // 투사체
+            towerDatabaseList_Ingame.itemList[i].Projectile_Speed = float.Parse(col[24]);
+            towerDatabaseList_Ingame.itemList[i].Projectile_Type = int.Parse(col[25]);
+            towerDatabaseList_Ingame.itemList[i].Projectile_Pf = Resources.Load<GameObject>(col[26]);
+
+            // 부가옵션
+            towerDatabaseList_Ingame.itemList[i].Destroy_Effect_Pf_Name = col[30];
+            towerDatabaseList_Ingame.itemList[i].Desc = col[33];
+            towerDatabaseList_Ingame.itemList[i].Sprite_TowerCard = Resources.Load<Sprite>("Sprites/" + col[34]);
+            towerDatabaseList_Ingame.itemList[i].Sprite_TowerProtrait = Resources.Load<Sprite>("Sprites/TowerPortrait" + col[35]);
+
+            towerDatabaseList_Ingame.itemList[i].AudioClip_Attack_Name = col[36];
+            towerDatabaseList_Ingame.itemList[i].AudioClip_Hit_Name = col[37];
+            towerDatabaseList_Ingame.itemList[i].AudioClip_Normal_Name = col[38];
+
         }
+
+        Debug.Log($"int rowSize : {rowSize}");
+        Debug.Log($"columnSize : {columnSize}");
     }
-
-    //public void SetCharactorDatas(string tsv)
-    //{
-    //    string[] row = tsv.Split('\n');
-    //    int rowSize = row.Length;
-    //    int columnSize = row[0].Split('\t').Length;
-
-    //    for (int i = 0; i < rowSize; i++)
-    //    {
-    //        string[] col = row[i].Split('\t');   // row  = 고양이,강아지,병아리
-    //        // col = [고양이, 강아지, 병아리]
-    //        ghosts[i].id = int.Parse(col[0]);
-    //        ghosts[i].hp = int.Parse(col[1]);
-    //        ghosts[i].strength = int.Parse(col[2]);
-    //        ghosts[i].xpReward = int.Parse(col[3]);
-    //        Debug.Log($"monster{ghosts[i].id} info // hp :{ghosts[i].hp}, strength : {ghosts[i].strength}, xpReward : {ghosts[i].xpReward}");
-    //    }
-
-    //    Debug.Log($"int rowSize : {rowSize}");
-    //    Debug.Log($"columnSize : {columnSize}");
-    //}
 }
