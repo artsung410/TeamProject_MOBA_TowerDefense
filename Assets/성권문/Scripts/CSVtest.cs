0d3b00dece44531a9c6c6f50b843c4ca55f92159
SungKwonMoon <zpzpskm@naver.com>
2022-12-01 09:59:43 +0900

    desc 확장공사

--- Assets/성권문/Scripts/CSVtest.cs
+++ Assets/성권문/Scripts/CSVtest.cs
@@ -90,7 +90,7 @@ public class CSVtest : MonoBehaviour
 
     #region DescParsing
 
-    private const string descURL = "https://docs.google.com/spreadsheets/d/1ta3EbfGEC9NswgOeCqHaI25BO9sPvpc2/export?format=tsv&range=A3:D57";
+    private const string descURL = "https://docs.google.com/spreadsheets/d/1ta3EbfGEC9NswgOeCqHaI25BO9sPvpc2/export?format=tsv&range=A3:D109";
 
     // Ű����� ID, List���� Name KoTooltip, EnTooltip�� ����
     Dictionary<int, List<string>> descDic = new Dictionary<int, List<string>>();
